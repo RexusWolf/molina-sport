3159ff893a5fe37a1a361fb55952c280a50e5d3a
rexuswolf <francisjmp99@gmail.com>
2020-08-12 12:36:24 +0200

    Not outlined

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -40,14 +40,14 @@ export default function Home() {
     <Grid className={classes.root} item container alignItems="center" justify="space-around">
       <Grid className={classes.grid} item xs={12} sm={6}>
         <Link className={classes.link} to="/catalogo">
-          <Button fullWidth variant="outlined" className={`${classes.button} ${classes.catalogButton}`} >
+          <Button fullWidth className={`${classes.button} ${classes.catalogButton}`} >
             <Typography variant="h6">CATÁLOGO</Typography>
           </Button>
         </Link>
       </Grid>
       <Grid className={classes.grid} item xs={12} sm={6}>
         <Link className={classes.link} to="/info">
-          <Button fullWidth variant="outlined" className={`${classes.button} ${classes.infoButton}`} >
+          <Button fullWidth className={`${classes.button} ${classes.infoButton}`} >
             <Typography variant="h6">INFORMACIÓN</Typography>
           </Button>
         </Link>
