bfdf17b7a2e121052d3fe4b0775d5debade51f8d
rexuswolf <francisjmp99@gmail.com>
2020-08-12 12:35:48 +0200

    Pretty and fresh design

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles, Grid, Typography, Button } from '@material-ui/core';
+import { makeStyles, Theme, createStyles, Grid, Typography, Button, withTheme } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    root: {
+      background: 'linear-gradient(to right, #ffd89b, #19547b)',
+    },
+    grid: {
+      height: '100%',
+    },
     button: {
-      height: 320,
-      width: 540,
+      height: '100%',
     },
     link: {
       textDecoration: 'none',
     },
     catalogButton: {
-      backgroundColor: theme.palette.primary.main,
+      background: 'linear-gradient(to right, #396afc, #2948ff)',
       color: 'white',
+      "&:hover": {
+        background: 'linear-gradient(to right, #06beb6, #48b1bf)'
+      }
     },
     infoButton: {
-      backgroundColor: theme.palette.secondary.main,
+      background: 'linear-gradient(to right, #f7971e, #ffd200)',
+      color: 'white',
+      "&:hover": {
+        background: 'linear-gradient(to right, #f2994a, #f2c94c)',
+      }
     }
   }),
 );
@@ -25,17 +37,17 @@ export default function Home() {
   const classes = useStyles();
 
   return (
-    <Grid item container alignItems="center" justify="space-around">
-      <Grid item xs={5}>
+    <Grid className={classes.root} item container alignItems="center" justify="space-around">
+      <Grid className={classes.grid} item xs={12} sm={6}>
         <Link className={classes.link} to="/catalogo">
-          <Button variant="outlined" className={`${classes.button} ${classes.catalogButton}`} >
+          <Button fullWidth variant="outlined" className={`${classes.button} ${classes.catalogButton}`} >
             <Typography variant="h6">CATÁLOGO</Typography>
           </Button>
         </Link>
       </Grid>
-      <Grid item xs={5}>
+      <Grid className={classes.grid} item xs={12} sm={6}>
         <Link className={classes.link} to="/info">
-          <Button variant="outlined" className={`${classes.button} ${classes.infoButton}`} >
+          <Button fullWidth variant="outlined" className={`${classes.button} ${classes.infoButton}`} >
             <Typography variant="h6">INFORMACIÓN</Typography>
           </Button>
         </Link>
