fe4b55a01fa24e1eefa1b62a9575bfa839e2869e
rexuswolf <francisjmp99@gmail.com>
2020-08-12 14:38:17 +0200

    Improved navbar behaviour

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -4,7 +4,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import { makeStyles, createStyles, Theme } from '@material-ui/core';
+import { makeStyles, createStyles, Theme, Box } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,13 +13,14 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     link: {
       textDecoration: 'none',
+      flexGrow: 0.5,
     },
     navBar: {
       background: 'linear-gradient(to right, #0f0c29, #302b63, #24243e)',
     },
     font: {
       fontFamily: 'OPTIEdgar',
-    }
+    },
   }),
 );
 
@@ -31,11 +32,14 @@ export const NavBar: React.FC = () => {
         <Link to="/catalogo" className={classes.link}>
           <Button color="secondary"><Typography className={classes.font}>Catálogo</Typography></Button>
         </Link>
-        <Link to="/" className={`${classes.title} ${classes.link}`}>
-          <Typography className={classes.font} color="secondary" variant="h5">
-            NEUMÁTICOS MOLINA SPORT
+        <Box className={classes.title}
+          display={{ xs: "none", md: "block" }}>
+          <Link to="/" className={classes.link}>
+            <Typography className={classes.font} color="secondary" variant="h5">
+              NEUMÁTICOS MOLINA SPORT
           </Typography>
-        </Link>
+          </Link>
+        </Box>
         <Link to="/info" className={classes.link}>
           <Button color="secondary"><Typography className={classes.font}>Información</Typography></Button>
         </Link>
